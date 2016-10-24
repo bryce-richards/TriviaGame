fb37a31c6b30e65e882cdc1cad61d02fed97b69d
bryce-richards <brichards5@gmail.com>
2016-10-23 17:13:24 -0700

    sun oct-23

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,13 +2,11 @@
 var questions = [];
 // hints
 var hints;
-// boolean to determine if guess was correct or not
-var correctGuess;
 // counter of correct answers
 var correctGuesses;
 // counter of wrong answers
 var wrongGuesses;
-// counter of unasnwered questions
+// counter of unanswered questions
 var unansweredQuestions;
 // correct answer
 var correctAnswer;
@@ -26,13 +24,19 @@ var currentTime;
 var counter;
 // time left
 var timeLeft;
+// total number of questions
+var totalQuestions;
+// width of span
+var width;
 // div shortcuts
+var progressBarDiv = $(".progress");
 var questionDiv = $("#question");
 var questionNumberDiv = $("#questionNumber");
 var answerDiv = $(".answer");
 var correctAnswerDiv = $(".correct");
-var hintsDiv = $("#hintsLeft");
+var hintsSpan = $("#hintsLeft");
 var timeLeftDiv = $("#timeLeft");
+
 
 /* NEW GAME OBJECT */
 function newGame() {
@@ -118,7 +122,10 @@ function newGame() {
             "Answer3"
         ]
     }];
-
+    // get total number of questions
+    totalQuestions = questions.length;
+    // set width of progress bar updates
+    width = (1/totalQuestions) * 100;
     // set number of hints per game
     hints = 3;
     // reset questionNumber
@@ -131,26 +138,69 @@ function newGame() {
     wrongGuesses = 0;
     // reset unansweredGuesses counter
     unansweredGuesses = 0;
+    // add total hints to page
+    hintsSpan.text(hints);
 
+    // load first question
+    newQuestion(questions);
 
     // new question function
     function newQuestion(array) {
+        // reset time
+        timeLeftDiv.text("10 seconds");
+        // updated progress bar
+        function progressBar(answer) {
+            if (answer === true) {
+                progressBarDiv.append("<div class='progress-bar progress-bar-success' style='width: " + width + "%'></div>")
+            }
+            else if (answer === false) {
+                progressBarDiv.append("<div class='progress-bar progress-bar-danger' style='width: " + width + "%'></div>")
+            } else {
+                progressBarDiv.append("<div class='progress-bar progress-bar-warning' style='width: " + width + "%'></div>")
+            }
+        }
+        // timer function
+        function startTimer() {
+            counter = setInterval(timeDown, 1000);
+        }
+        // decrement function.
+        function timeDown() {
+            // decrease number by one of seconds by one.
+            timeLeft--;
+            // show the number of seconds left.
+            timeLeftDiv.text(timeLeft + " seconds");
+
+            // Once number hits zero...
+            if (timeLeft === 0){
+                // run the stop function.
+                timesUp();
+                // update progress bar
+                progressBar();
+            }
+        }
+        // stop function
+        function timesUp() {
+            // Clears our "counter" interval.
+            // We just pass the name of the interval
+            // to the clearInterval function.
+            clearInterval(counter);
+        }
+
         // set timer to 10 seconds
         timeLeft = 10;
         // increase question number by 1
-        questionNumber++;
+        questionNumber++;console.log(questionNumber);
         // add question number to page
         questionNumberDiv.text(questionNumber);
         // reset answer divs
         answerDiv.each(function(i) {
-            $(this).removeClass("correct").addClass("wrong");
+            $(this).removeClass("correct").addClass("wrong").css({pointerEvents: "auto"});
         });
         // start timer
-        // startTimer();
+        startTimer();
         // get question object
         currentQuestion = array[0];
         // currentQuestion = array[Math.floor(Math.random() * array.length)];
-        // remove question object from array
         // add question to html page
         questionDiv.html("<p>" + currentQuestion.question + "</p>");
         // pick a random number from 1-4 to add correct answer to
@@ -162,47 +212,33 @@ function newGame() {
             $(this).html("<p>" + currentQuestion.wrongAnswers[i] + "</p>");
         });
         answerDiv.click(function() {
+            timesUp();
             if ($(this).hasClass("correct")) {
-                alert("Yay!");
+                correctGuesses++;
+                correctGuess = true;
             } else {
-                alert("No...");
+                correctGuess = false;
+                console.log("wrong");
+                wrongGuesses++;
             }
+            progressBar(correctGuess);
+            answerDiv.each(function(i) {
+                $(this).css({pointerEvents: "none"});
+            });
+            // remove question object from array
+            array.splice(array.indexOf(currentQuestion), 1);
+            setTimeout(function() {
+                newQuestion(questions);
+            }, 2000);
         });
-        array.splice(array.indexOf(currentQuestion), 1);
+
 
     }
 
     // begin first question
-    newQuestion(questions);
 
 }
 newGame();
 
-// // timer function to decrease the time left every second
-// function startTimer() {
-//     counter = setInterval(timeDown, 1000);
-// }
-//
-// // The decremeent function.
-// function timeDown() {
-//     // Decrease number by one.
-//     timeLeft--;
-//     // Show the number in the #show-number tag.
-//     timeLeftDiv.text(timeLeft + " seconds");
-//
-//     // Once number hits zero...
-//     if (timeLeft === 0){
-//         // ...run the stop function.
-//         timesUp();
-//         // Alert the user that time is up.
-//         alert('Time Up!')
-//     }
-// }
-// // The stop function
-// function timesUp() {
-//     // Clears our "counter" interval.
-//     // We just pass the name of the interval
-//     // to the clearInterval function.
-//     clearInterval(counter);
-// }
+
 
--- assets/javascript/cardgame.js
+++
@@ -1,4 +0,0 @@
-/**
- * Created by brycerichards on 10/22/16.
- */
-values = [var A, var 2, var 3, var 4, var 5, var]
